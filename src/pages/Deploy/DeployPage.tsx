74d2872898c93d89d40af1b6cc8ab60683e46739
ChenNingRui <LittleLoliAnne@gmail.com>
2023-08-08 17:35:56 +0200

    Update DeployPage.tsx
    change the deployed name

--- src/pages/Deploy/DeployPage.tsx
+++ src/pages/Deploy/DeployPage.tsx
@@ -1,27 +1,33 @@
-import { Button, BUTTON_SIZE, Divider, Grid, Header } from "../../components"
-import { StyledDeployWrapper, StyledWrapper } from "./DeployPage.styles"
-import { useNavigate } from 'react-router-dom';
+import { Button, BUTTON_SIZE, Divider, Grid, Header } from "../../components";
+import { StyledDeployWrapper, StyledWrapper } from "./DeployPage.styles";
+import { useNavigate } from "react-router-dom";
 import { colIndexs, rowIndexs } from "../../types";
 import { DeployPanel } from "./organisms";
 import { useDeloyStore } from "./store/useDeployStore";
 
 export const DeployPage = () => {
-    const navigate = useNavigate();
-    const { cells, resetDeployPanel } = useDeloyStore()
+  const navigate = useNavigate();
+  const { cells, resetDeployPanel } = useDeloyStore();
 
-    return (
-        <StyledWrapper>
-            <Header title="Delpoy">
-                <Button value="back" size={BUTTON_SIZE.S} onClick={() => {
-                    resetDeployPanel()
-                    navigate(-1)
-                }}>Back</Button>
-            </Header>
-            <Divider />
-            <StyledDeployWrapper>
-                <Grid cells={cells} cols={colIndexs} rows={rowIndexs} />
-                <DeployPanel />
-            </StyledDeployWrapper>
-        </StyledWrapper>
-    )
-}
+  return (
+    <StyledWrapper>
+      <Header title="Deployed">
+        <Button
+          value="back"
+          size={BUTTON_SIZE.S}
+          onClick={() => {
+            resetDeployPanel();
+            navigate(-1);
+          }}
+        >
+          Back
+        </Button>
+      </Header>
+      <Divider />
+      <StyledDeployWrapper>
+        <Grid cells={cells} cols={colIndexs} rows={rowIndexs} />
+        <DeployPanel />
+      </StyledDeployWrapper>
+    </StyledWrapper>
+  );
+};
